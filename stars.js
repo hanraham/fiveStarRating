06fa71a48352be65a8bb0b4cf7fcc9d44c0f35c5
amh-coding <a.hanrahan38a@gmail.com>
2022-03-16 10:06:46 -0400

    Trying out the docReady function instead of defer/async

--- stars.js
+++ stars.js
@@ -1,7 +1,15 @@
-// docReady(function () {
+function docReady(fn) {    
+    // see if DOM is already available
+    if (document.readyState === "complete" || document.readyState === "interactive") {
+        // call on next available tick        
+        setTimeout(fn, 1);    
+    } 
+    else {  document.addEventListener("DOMContentLoaded", fn);  }
+}
+
+docReady(function () {
     // DOM is loaded and ready for manipulation here
-
-
+    
     document.querySelector("#star0").onclick = function() { ratingStars(0); };
     document.querySelector("#star1").onclick = function() { ratingStars(1); };
     document.querySelector("#star2").onclick = function() { ratingStars(2); };
@@ -20,5 +28,4 @@
             }
         }
     };
-
-// });
+});
